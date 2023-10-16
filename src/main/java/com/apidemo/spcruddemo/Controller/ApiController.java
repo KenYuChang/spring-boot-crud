582fa6133f8524c1e9dad2207b41f71979ce863f
KenYuChang <justin0915justin0915@gmail.com>
2023-10-16 12:30:54 +0800

    getCustomerDetails api testing

--- src/main/java/com/apidemo/spcruddemo/Controller/ApiController.java
+++ src/main/java/com/apidemo/spcruddemo/Controller/ApiController.java
@@ -1,12 +1,21 @@
 package com.apidemo.spcruddemo.Controller;
+import com.apidemo.spcruddemo.Models.Customer;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/customer")
 public class ApiController {
 
-    @GetMapping(value = "/")
+    @GetMapping("")
     public String getPage() {
         return "Hello World";
+    }
+
+    @GetMapping("{customerId}")
+    public Customer getCustomerDetails(String customerId) {
+        return new Customer("C1",
+                "Ken", "Taipei", "0952789345");
     }
 }
