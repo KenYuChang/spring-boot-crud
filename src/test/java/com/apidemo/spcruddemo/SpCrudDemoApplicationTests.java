d2189553c3b7a0b803b68db99dbb2dab2e469f84
KenYuChang <justin0915justin0915@gmail.com>
2023-10-16 11:20:45 +0800

    project init

---
+++ src/test/java/com/apidemo/spcruddemo/SpCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apidemo.spcruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
